fd82d0dcfa102759e7f8e04bbf61a20d399e050c
Mak Moinee <titus.silver.ph@gmail.com>
2023-06-28 23:22:38 +0800

    Initial commit

---
+++ library/src/main/java/com/github/MakMoinee/library/MapForm.java
@@ -0,0 +1,13 @@
+package com.github.MakMoinee.library;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+public class MapForm {
+    public static Map<String, Object> convertObjectToMap(Object data) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> param = mapper.convertValue(data, Map.class);
+        return param;
+    }
+}
