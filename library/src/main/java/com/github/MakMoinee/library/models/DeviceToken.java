a8d4205be137507cd66559ad16bf34c9f765e584
Mak Moinee <titus.silver.ph@gmail.com>
2023-12-27 23:48:17 +0800

    update code

---
+++ library/src/main/java/com/github/MakMoinee/library/models/DeviceToken.java
@@ -0,0 +1,49 @@
+package com.github.MakMoinee.library.models;
+
+import lombok.Data;
+
+@Data
+public class DeviceToken {
+    private String docID;
+    private  String userID;
+    private String deviceToken;
+
+    public DeviceToken() {
+    }
+
+    public DeviceToken(DeviceTokenBuilder builder) {
+        this.docID = builder.docID;
+        this.deviceToken = builder.deviceToken;
+        this.userID = builder.userID;
+    }
+
+    public static class DeviceTokenBuilder {
+        private String docID;
+        private String deviceToken;
+        private  String userID;
+
+        public DeviceTokenBuilder() {
+        }
+
+        public DeviceTokenBuilder setDocID(String docID) {
+            this.docID = docID;
+            return this;
+        }
+
+        public DeviceTokenBuilder setDeviceToken(String deviceToken) {
+            this.deviceToken = deviceToken;
+            return this;
+        }
+
+        public DeviceTokenBuilder setUserID(String userID) {
+            this.userID = userID;
+            return this;
+        }
+
+        public DeviceToken build() {
+            return new DeviceToken(this);
+        }
+
+
+    }
+}
