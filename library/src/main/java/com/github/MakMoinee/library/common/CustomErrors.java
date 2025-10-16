a39d2fc7a8990ffefe1eabbf0fc19c1536e9aae8
Mak Moinee <titus.silver.ph@gmail.com>
2025-10-16 21:18:19 +0800

    update code

---
+++ library/src/main/java/com/github/MakMoinee/library/common/CustomErrors.java
@@ -0,0 +1,20 @@
+package com.github.MakMoinee.library.common;
+
+import android.util.Log;
+
+public class CustomErrors {
+    public static <T> void error(T any) {
+        if (any == null) {
+            Log.e("CustomErrors", "Unknown error occurred.");
+            return;
+        }
+
+        if (any instanceof Exception e) {
+            Log.e("CustomErrors", e.getMessage());
+        } else if (any instanceof String s) {
+            Log.e("CustomErrors", s);
+        } else {
+            Log.e("CustomErrors", any.toString());
+        }
+    }
+}
